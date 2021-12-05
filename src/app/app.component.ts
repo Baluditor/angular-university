ecc1aa21a4d65b8b8672ae20ea275e9cc4e800a0
Balazs Farkas <farkas.krisztian.balazs@gmail.com>
2021-12-05 13:01:33 +0100

    Further inspection of @ViewChild

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import {Component, ViewChild} from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 import {COURSES} from '../db-data';
 import {CourseCardComponent} from './course-card/course-card.component';
 import {Course} from './model/course';
@@ -14,12 +14,23 @@ export class AppComponent {
   
   startDate = new Date();
   
-  @ViewChild(CourseCardComponent)
+  // To get a reference to an HTML element, not the course card component
+  @ViewChild('cardRef', {read: ElementRef})
   card: CourseCardComponent;
+  
+  // Get a reference for the course card component instance
+  @ViewChild('cardRef2')
+  card2: CourseCardComponent;
+  
+  // Get a reference to a native HTML element.
+  @ViewChild('container')
+  containerDiv: ElementRef;
   
   onCourseSelected(course: Course): void {
     console.log('App component - click event bubbled', course);
     console.log(this.card);
+    console.log(this.card2);
+    console.log(this.containerDiv);
   }
   
 }
