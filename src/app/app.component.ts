e8a5d88f8322844fc61abecfbea5552ba2821844
Balazs Farkas <farkas.krisztian.balazs@gmail.com>
2021-12-05 07:20:19 +0100

    Template binding example.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'angular-beginner';
+  
+  data = {
+    title: 'Angular Core Deep dive'
+  }
+  
+  onKeyUp(value: string): void {
+    this.data.title = value;
+  }
 }
