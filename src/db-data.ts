0beb8a3656706319a2af46a9f97901decd6ac12d
Balazs Farkas <farkas.krisztian.balazs@gmail.com>
2021-12-05 11:50:08 +0100

    Use ng-container and ng switch.

--- src/db-data.ts
+++ src/db-data.ts
@@ -8,7 +8,6 @@ export const COURSES: any = [
     // iconUrl:
     // 'https://s3-us-west-1.amazonaws.com/angular-university/course-images/angular-core-in-depth-small.png',
     longDescription: 'A detailed walk-through of the most important part of Angular - the Core and Common modules',
-    category: 'INTERMEDIATE',
     lessonsCount: 10,
   },
   {
