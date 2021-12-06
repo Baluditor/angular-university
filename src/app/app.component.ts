5e91d40fd797cdfd83104349652409e61111b266
Balazs Farkas <farkas.krisztian.balazs@gmail.com>
2021-12-06 10:37:48 +0100

    Intro for attribute directives

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,30 @@
-import {AfterViewInit, Component, ElementRef, QueryList, ViewChild, ViewChildren} from '@angular/core';
+import {AfterViewInit, Component, ElementRef, QueryList, ViewChildren} from '@angular/core';
 import {COURSES} from '../db-data';
-import {Course} from './model/course';
 import {CourseCardComponent} from './course-card/course-card.component';
+import {Course} from './model/course';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements AfterViewInit {
-
-    courses = COURSES;
-
-
-    @ViewChildren(CourseCardComponent, {read: ElementRef})
-    cards : QueryList<ElementRef>;
-
-
-    constructor() {
-
-    }
-
-    ngAfterViewInit() {
-
-    }
-
-    onCourseSelected(course:Course) {
-
-    }
-
+  
+  courses: Array<Course> = COURSES;
+  
+  @ViewChildren(CourseCardComponent, {read: ElementRef})
+  cards: QueryList<ElementRef>;
+  
+  constructor() {
+  
+  }
+  
+  ngAfterViewInit() {
+  
+  }
+  
+  onCourseSelected(course: Course) {
+  
+  }
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,25 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
-import { AppComponent } from './app.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { CourseCardComponent } from './course-card/course-card.component';
-import { CourseImageComponent } from './course-image/course-image.component';
+import {AppComponent} from './app.component';
+import {CourseCardComponent} from './course-card/course-card.component';
+import {CourseImageComponent} from './course-image/course-image.component';
+import {HighlightedDirective} from './directives/highlighted.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     CourseCardComponent,
-    CourseImageComponent
+    CourseImageComponent,
+    HighlightedDirective,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {
+}
