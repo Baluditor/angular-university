60a1d579c972815970e83a33f2badc75688c1478
Balazs Farkas <farkas.krisztian.balazs@gmail.com>
2021-12-07 09:53:17 +0100

    Start working with structural directives. Create a new structural directive.

---
+++ src/app/directives/ngx-unless.directive.ts
@@ -0,0 +1,12 @@
+import {Directive} from '@angular/core';
+
+@Directive({
+  // Save syntax as attribute directives
+  selector: '[ngxUnless]',
+})
+export class NgxUnlessDirective {
+  
+  constructor() {
+  }
+  
+}
